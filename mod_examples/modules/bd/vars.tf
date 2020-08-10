6e90a97424b5d1b9af2be06fb560183d3e7100f7
cobedien <cobedien@cisco.com>
2020-08-10 13:57:47 -0400

    Create vars.tf

---
+++ mod_examples/modules/bd/vars.tf
@@ -0,0 +1,15 @@
+variable "tenant_id" {
+    default = ""
+    }
+
+variable "vrf_id" {
+    default = ""
+    }
+
+variable "bd" {
+    default = ""
+    }
+
+variable "ip" {
+    default = ""
+    }
