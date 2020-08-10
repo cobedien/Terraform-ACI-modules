3c65e5424a661bc4330a6b459f16d795ddfc5558
cobedien <cobedien@cisco.com>
2020-08-10 13:57:24 -0400

    Create bd.tf

---
+++ mod_examples/modules/bd/bd.tf
@@ -0,0 +1,10 @@
+resource "aci_bridge_domain" "bd" {
+    tenant_dn             = var.tenant_id
+    relation_fv_rs_ctx    = var.vrf_id
+    name                  = var.bd
+    }
+
+resource "aci_subnet" "bd_subnet" {
+    bridge_domain_dn      = aci_bridge_domain.bd.id
+    ip                    = var.ip
+    }
