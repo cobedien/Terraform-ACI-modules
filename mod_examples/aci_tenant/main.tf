173c706f63fc5642d425b0c8e616b0d6ec5b2292
cobedien <cobedien@cisco.com>
2020-08-10 13:55:54 -0400

    Update main.tf

--- mod_examples/aci_tenant/main.tf
+++ mod_examples/aci_tenant/main.tf
@@ -14,22 +14,22 @@ module "my_tenant" {
 module "my_vrf" {
     source                = "../modules/vrf"
     vrf                   = "pn_26"
-    tenant_id             = "\${module.my_tenant.tenant_id}"
+    tenant_id             = "${module.my_tenant.tenant_id}"
     }
 
 module "my_bd_app" {
     source                = "../modules/bd"
     bd                    = "pod26_app"
     ip                    = "5.1.1.1/24"
-    tenant_id             = "\${module.my_tenant.tenant_id}"
-    vrf_id                = "\${module.my_vrf.vrf_id}"
+    tenant_id             = "${module.my_tenant.tenant_id}"
+    vrf_id                = "${module.my_vrf.vrf_id}"
     }
 
 module "my_bd_web" {
     source                = "../modules/bd"
     bd                    = "pod26_web"
     ip                    = "6.1.1.1/24"
-    tenant_id             = "\${module.my_tenant.tenant_id}"
-    vrf_id                = "\${module.my_vrf.vrf_id}"
+    tenant_id             = "${module.my_tenant.tenant_id}"
+    vrf_id                = "${module.my_vrf.vrf_id}"
     }
 EOF
